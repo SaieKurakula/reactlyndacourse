c2524a94d0469847111dc43261b97302fd56eecf
Saie Kurakula <saiekurakula@gmail.com>
2021-01-10 19:57:57 -0500

    first 2 sections of lynda course

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import restaurant from './restaurant.gif'
+
+function Header(props) {
+  return (
+    <header>
+      <h1>{props.name}'s Kitchen</h1>
+    </header>
+  );
+
+}
+
+function Main(props) {
+  return(
+    <section>
+      <p>We serve the most {props.adjective} food around!</p>
+      <img src={restaurant} alt="Ninja turtles celebrating"/>
+      <img src="https:github.com/saiekurakula.png" alt="Saie Kurakula"/>
+      <ul>
+        {props.dishes.map((dish) =>(
+          <li key={dish.id}>{dish.title}</li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+function Footer(props) {
+  return(
+    <footer>
+      <p>Copywright: {props.year}</p>
+    </footer>
+  );
+}
+
+const dishes = [
+  "Mac & Cheese",
+  "Salmon",
+  "Pork Chops",
+  "Potato"
+];
+
+const dishObjects = dishes.map((dish, i)=>({id: i, title: dish}))
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Saie" />
+      <Main adjective="amazing" dishes={dishObjects}/>
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
