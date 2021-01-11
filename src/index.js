89218dbc314387dbf1a3d22c5feca32163e5be2f
Saie Kurakula <saiekurakula@gmail.com>
2021-01-11 16:01:48 -0500

    array and object destructuring

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App'
 
+const [mostImportantItem, second] = ["boots", "tent", "headlamp"];
+//const [, , light] = ["boots", "tent", "headlamp"];
+console.log(mostImportantItem, second);
+//console.log(light);
+
+// Object vs array destructuring:
+
 ReactDOM.render(
     <App authorized={true} />,
   document.getElementById('root')
