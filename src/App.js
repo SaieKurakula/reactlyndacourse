75a16e3ff4de2871477ae5c5497a0c6e8b5adfe4
Saie Kurakula <saiekurakula@gmail.com>
2021-01-10 20:07:01 -0500

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,55 +1,21 @@
 import './App.css';
-import restaurant from './restaurant.gif'
 
-function Header(props) {
-  return (
-    <header>
-      <h1>{props.name}'s Kitchen</h1>
-    </header>
-  );
-
+function SecretComponent() {
+  return <h1>Super secret info for authorized users only</h1>
 }
 
-function Main(props) {
-  return(
-    <section>
-      <p>We serve the most {props.adjective} food around!</p>
-      <img src={restaurant} alt="Ninja turtles celebrating"/>
-      <img src="https:github.com/saiekurakula.png" alt="Saie Kurakula"/>
-      <ul>
-        {props.dishes.map((dish) =>(
-          <li key={dish.id}>{dish.title}</li>
-        ))}
-      </ul>
-    </section>
-  );
+function RegularComponent(){
+  return <h1>Everyone else can see this component.</h1>
 }
 
-function Footer(props) {
-  return(
-    <footer>
-      <p>Copywright: {props.year}</p>
-    </footer>
-  );
-}
+function App(props) {
+  if(props.authorized) {
+    return <SecretComponent />;
+  }
+  else {
+    return <RegularComponent />;
+  }
 
-const dishes = [
-  "Mac & Cheese",
-  "Salmon",
-  "Pork Chops",
-  "Potato"
-];
-
-const dishObjects = dishes.map((dish, i)=>({id: i, title: dish}))
-
-function App() {
-  return (
-    <div className="App">
-      <Header name="Saie" />
-      <Main adjective="amazing" dishes={dishObjects}/>
-      <Footer year={new Date().getFullYear()} />
-    </div>
-  );
 }
 
 export default App;
